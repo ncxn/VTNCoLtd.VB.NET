4c7e233b971042a70cb51685b6394bb7ce26fb60
ncxn <ncxn@outlook.com>
2018-12-25 21:24:29 +0700

    add project

---
+++ DancePassion/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FluentDesignFormContainer1 = New DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer()
+        Me.AccordionControl1 = New DevExpress.XtraBars.Navigation.AccordionControl()
+        Me.AccordionControlElement1 = New DevExpress.XtraBars.Navigation.AccordionControlElement()
+        Me.FluentDesignFormControl1 = New DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl()
+        CType(Me.AccordionControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FluentDesignFormControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'FluentDesignFormContainer1
+        '
+        Me.FluentDesignFormContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FluentDesignFormContainer1.Location = New System.Drawing.Point(202, 30)
+        Me.FluentDesignFormContainer1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.FluentDesignFormContainer1.Name = "FluentDesignFormContainer1"
+        Me.FluentDesignFormContainer1.Size = New System.Drawing.Size(420, 330)
+        Me.FluentDesignFormContainer1.TabIndex = 0
+        '
+        'AccordionControl1
+        '
+        Me.AccordionControl1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.AccordionControl1.Elements.AddRange(New DevExpress.XtraBars.Navigation.AccordionControlElement() {Me.AccordionControlElement1})
+        Me.AccordionControl1.Location = New System.Drawing.Point(0, 30)
+        Me.AccordionControl1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.AccordionControl1.Name = "AccordionControl1"
+        Me.AccordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Touch
+        Me.AccordionControl1.Size = New System.Drawing.Size(202, 330)
+        Me.AccordionControl1.TabIndex = 1
+        Me.AccordionControl1.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu
+        '
+        'AccordionControlElement1
+        '
+        Me.AccordionControlElement1.Name = "AccordionControlElement1"
+        Me.AccordionControlElement1.Text = "Element1"
+        '
+        'FluentDesignFormControl1
+        '
+        Me.FluentDesignFormControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.FluentDesignFormControl1.FluentDesignForm = Me
+        Me.FluentDesignFormControl1.Location = New System.Drawing.Point(0, 0)
+        Me.FluentDesignFormControl1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.FluentDesignFormControl1.Name = "FluentDesignFormControl1"
+        Me.FluentDesignFormControl1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.FluentDesignFormControl1.Size = New System.Drawing.Size(622, 30)
+        Me.FluentDesignFormControl1.TabIndex = 2
+        Me.FluentDesignFormControl1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(622, 360)
+        Me.ControlContainer = Me.FluentDesignFormContainer1
+        Me.Controls.Add(Me.FluentDesignFormContainer1)
+        Me.Controls.Add(Me.AccordionControl1)
+        Me.Controls.Add(Me.FluentDesignFormControl1)
+        Me.FluentDesignFormControl = Me.FluentDesignFormControl1
+        Me.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.Name = "Form1"
+        Me.NavigationControl = Me.AccordionControl1
+        Me.Text = "Form1"
+        CType(Me.AccordionControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FluentDesignFormControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents FluentDesignFormContainer1 As DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer
+    Friend WithEvents AccordionControl1 As DevExpress.XtraBars.Navigation.AccordionControl
+    Friend WithEvents AccordionControlElement1 As DevExpress.XtraBars.Navigation.AccordionControlElement
+    Friend WithEvents FluentDesignFormControl1 As DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl
+End Class
---
+++ DancePassion/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+        Dim tb As DataTable
+        Dim svr As New MYSQLSERVER
+        tb = svr.Get_Table("select * from product_type")
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+End Class
