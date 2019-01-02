5982c36eb3bdba052f49f98983ae90fdfe86f989
ncxn <ncxn@outlook.com>
2019-01-02 09:17:32 +0700

    get

--- DancePassion/My Project/Application.Designer.vb
+++ DancePassion/My Project/Application.Designer.vb
@@ -25,14 +25,19 @@ Namespace My
         Public Sub New()
             MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
             Me.IsSingleInstance = false
-            Me.EnableVisualStyles = true
-            Me.SaveMySettingsOnExit = true
+            Me.EnableVisualStyles = false
+            Me.SaveMySettingsOnExit = false
             Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterAllFormsClose
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.DancePassion.Form1
+        End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.DancePassion.SplashScreen
         End Sub
     End Class
 End Namespace
